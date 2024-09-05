2f35e164ff2397f6e64b6e7fcc65c57d702e02cc
Sjelly23 <4210759@schoolsnet.act.edu.au>
2024-09-05 12:37:01 +1000

    make score counter workie

--- clicky-clicky/script.js
+++ clicky-clicky/script.js
@@ -1,7 +1,19 @@
 //define variables
 let moneyz = 0;
+let scoreCounter = document.getElementById("scoreCounter");
+
+function updateScoreCounter(){
+    scoreCounter.innerHTML = "score: " + String(moneyz);
+}
 
 function getMoneyz(moneyzToGet){
     moneyz += moneyzToGet;
-    alert(moneyz)
+    updateScoreCounter();
 }
+
+function init(){
+    updateScoreCounter();
+}
+
+//initilize the game
+init();
