4a807cbac35c60c416c8c560982b50e3241161df
Sjelly23 <4210759@schoolsnet.act.edu.au>
2024-09-05 13:38:56 +1000

    add incrementalCost

--- clicky-clicky/script.js
+++ clicky-clicky/script.js
@@ -2,6 +2,7 @@
 let moneyz = 0;
 let scoreCounter = document.getElementById("scoreCounter");
 let clickPower = 1;
+let clickPowerCostIncremental = [5, 1.4];
 
 function updateScoreCounter(){
     scoreCounter.innerHTML = "score: " + String(moneyz);
@@ -16,9 +17,10 @@ function getClickPower(clickPowerToGet){
     clickPower += clickPowerToGet;
 }
 
-function buyThing(cost, callback){
-    if(moneyz >= cost){
-        modifyMoneyz(-cost);
+function buyThing(incrementalCost, callback){
+    if(moneyz >= incrementalCost[0]){
+        modifyMoneyz(-incrementalCost[0]);
+        incrementalCost[0] *= incrementalCost[1];
         callback();
     }
 }
